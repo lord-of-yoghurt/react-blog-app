51b541318224ad88dee79a28b5818f9c4d14f121
David Ordovskiy <david.ordovsky@gmail.com>
2017-09-28 15:47:53 -0400

    implement handling post submittal, including async redirecting to index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=davidlovescoding';
@@ -10,6 +11,16 @@ export function fetchPosts() {
 
   return {
     type: FETCH_POSTS,
+    payload: req
+  };
+}
+
+export function createPost(values, callback) {
+  const req = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback());
+
+  return {
+    type: CREATE_POST,
     payload: req
   };
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
   renderField(field) {
     const { meta: { touched, error } } = field;
     const className = `form-group ${touched && error ? 'has-danger' : ''}`;
-    
+
     return (
       <div className={className}>
         <input
@@ -27,7 +31,9 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    console.log(values);
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
@@ -51,6 +57,7 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
@@ -81,4 +88,6 @@ export default reduxForm({
   validate: validate,
   // name of the form - has to be unique
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null, { createPost })(PostsNew)
+);
